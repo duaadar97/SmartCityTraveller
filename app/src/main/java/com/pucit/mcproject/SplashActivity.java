6582daa85bb654a8e1d18c8abfb4a03c2e93383a
duaadar26 <bcsf16a003@pucit.edu.pk>
2019-12-22 14:59:21 +0500

    splash screen

--- app/src/main/java/com/pucit/mcproject/SplashActivity.java
+++ app/src/main/java/com/pucit/mcproject/SplashActivity.java
@@ -23,7 +23,7 @@ public class SplashActivity extends AppCompatActivity {
                 Intent intent=new Intent(SplashActivity.this,MainActivity.class);
                 startActivity(intent);
             }
-        },2500);
+        },2000);
     }
     }
 
